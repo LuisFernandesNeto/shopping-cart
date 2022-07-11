4b7fe65043521165f98f59ffeba2f505d723e873
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-07-11 18:46:59 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,16 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (item) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+
+  try {
+  const request = await fetch(url);
+  const data = await request.json();
+  return data;
+  } catch (error) {
+    return 'You must provide an url';
+  }
 };
+
+fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,26 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
+const item = require('../mocks/item');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('It should be a function', () => {
+    expect(typeof fetchProducts).toEqual('function');
+  });
+  it('Execute a função fetchProducts com o argumento `computador` e teste se fetch foi chamada', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it('Teste se, ao chamar a função fetchProducts com o argumento `computador`, a função fetch utiliza o endpoint `https://api.mercadolibre.com/sites/MLB/search?q=computador`', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(`https://api.mercadolibre.com/sites/MLB/search?q=computador`);
+  });
+  it('fetchProducts with argument `computador` should return equal to `ComputadorSearch`', async () => {
+    const computer = await fetchProducts('computador');
+    expect(computer).toEqual(computadorSearch);
+  });
+  it('It should return `You must provide an url` if fetchProducts does not have an argument', async () => {
+    const retorno = await fetchProducts();
+    expect(retorno).toEqual('You must provide an url');
+  });
 });
